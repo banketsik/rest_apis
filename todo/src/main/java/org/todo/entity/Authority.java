d567dfef2dfdaa743826f1a96e79aed665b5e4c1
banketsik <aztocge@gmail.com>
2026-01-21 14:27:01 +0300

    Create POJO objects

---
+++ todo/src/main/java/org/todo/entity/Authority.java
@@ -0,0 +1,20 @@
+package org.todo.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class Authority implements GrantedAuthority {
+
+    private String authority;
+
+    public Authority() {
+    }
+
+    public Authority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+}
---
+++ todo/src/main/java/org/todo/entity/Todo.java
@@ -0,0 +1,80 @@
+package org.todo.entity;
+
+import jakarta.persistence.*;
+
+@Table(name = "todos")
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "priority", nullable = false)
+    private Integer priority;
+
+    @Column(name = "complete", nullable = false)
+    private Boolean complete;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    public Todo() {
+    }
+
+    public Todo(String title, String description, Integer priority, Boolean complete, User owner) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.complete = complete;
+        this.owner = owner;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Boolean getComplete() {
+        return complete;
+    }
+
+    public void setComplete(Boolean complete) {
+        this.complete = complete;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+}
