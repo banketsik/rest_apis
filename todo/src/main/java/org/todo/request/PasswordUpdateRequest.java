b566b0f670e5ccaaf7cda4fa0c0c25de10da8e32
banketsik <aztocge@gmail.com>
2026-01-22 08:46:30 +0300

    Add validation for request

--- todo/src/main/java/org/todo/request/PasswordUpdateRequest.java
+++ todo/src/main/java/org/todo/request/PasswordUpdateRequest.java
@@ -1,4 +1,49 @@
 package org.todo.request;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class PasswordUpdateRequest {
+
+    @NotEmpty(message = "Old password is mandatory")
+    @Size(min = 5, max = 30, message = "Old password must be at least 5 characters long")
+    private String oldPassword;
+
+    @NotEmpty(message = "New password is mandatory")
+    @Size(min = 5, max = 30, message = "New password must be at least 5 characters long")
+    private String newPassword;
+
+    @NotEmpty(message = "Confirmed password is mandatory")
+    @Size(min = 5, max = 30, message = "Confirmed password must be at least 5 characters long")
+    private String confirmedPassword;
+
+    public PasswordUpdateRequest(String oldPassword, String newPassword, String confirmedPassword) {
+        this.oldPassword = oldPassword;
+        this.newPassword = newPassword;
+        this.confirmedPassword = confirmedPassword;
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getConfirmedPassword() {
+        return confirmedPassword;
+    }
+
+    public void setConfirmedPassword(String confirmedPassword) {
+        this.confirmedPassword = confirmedPassword;
+    }
 }
